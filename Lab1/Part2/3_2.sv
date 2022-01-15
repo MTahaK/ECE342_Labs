e93d7bfb2089baf805f99e053121c795fc642509
MTahaK <taha.khokhar@mail.utoronto.ca>
2022-01-15 18:44:32 -0500

    Added code for part 3.2 in 3_2.sv

--- Lab1/Part2/3_2.sv
+++ Lab1/Part2/3_2.sv
@@ -7,3 +7,19 @@ module upcount15
     output o_last,
 
 );
+    logic [3:0] count;
+    always_ff @(posedge clk) begin
+        if (sreset) count <= 0;
+        else begin
+            if (i_enable) begin
+                if (o_last) count<=0;
+                else count <= count + 1;
+            end
+        end
+    end
+
+    assign o_val = count;
+    assign o_last = (count == 4'b1111);
+
+
+endmodule
