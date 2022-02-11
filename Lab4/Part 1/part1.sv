5ac17135aa1a74b2e80956b6fa708759243c585f
JinwooCheon-GitHub <82179219+JinwooCheon-GitHub@users.noreply.github.com>
2022-02-11 02:50:34 -0500

    part 1 fixing

--- Lab4/Part 1/part1.sv
+++ Lab4/Part 1/part1.sv
@@ -2,7 +2,22 @@ module part1(
 	input [31:0] X,
 	input [31:0] Y,
 	output [31:0] result,
-	output zero, underflow, overflow, nan
+	output logic [7:0] exponent_x,
+	output logic [7:0] exponent_y,
+	output logic [8:0] exponent_with_127,
+	output logic [23:0] mantissa_x_hidden,
+	output logic [23:0] mantissa_y_hidden,
+	output logic [47:0] mantissa,
+	output logic [22:0] mantissa_truncated,
+	output logic [1:0] mantissa_hidden,
+	output logic [22:0] mantissa_final,
+	output logic [8:0] exponent_final,
+	output logic [22:0] mantissa_x,
+	output logic [22:0] mantissa_y,
+	output logic zero, 
+	output logic underflow, 
+	output logic overflow, 
+	output logic nan
 );
 
 	// 1. XOR the sign bits of X & Y to get the sign of the result.
@@ -12,22 +27,31 @@ module part1(
 	
 	// 2. Add the exponents of X & Y, then subtract 127 since we are adding 2*127 to the final number.
 	
-	logic [7:0] exponent;
-	assign exponent  = X[30:23] + Y[30:23] - 127;
+	//logic [8:0] exponent;	// make the exponent 9bit to check for special cases: for overflow
+	//logic [7:0] exponent_x;	// exponent_x and exponent_y created for special cases check
+	//logic [7:0] exponent_y; 
+	assign exponent_x = X[30:23];
+	assign exponent_y = Y[30:23];
+	//assign exponent  = exponent_x + exponent_y - 127;
+	assign exponent_with_127 = exponent_x + exponent_y;
 	
 	// 3. Multiply the (23-bit) mantissas of X & Y . Along with the hidden 1 in each mantissa, this results in a 48-bit product.
 	
-	logic [47:0] mantissa;
-	logic [23:0] mantissa_x;
-	logic [23:0] mantissa_y;
-	assign mantissa_x = {1'b1, X[22:0]};
-	assign mantissa_y = {1'b1, Y[22:0]};
-	assign mantissa =  mantissa_x * mantissa_y;
+	//logic [47:0] mantissa;
+	//logic [23:0] mantissa_x_hidden; //24 bit mantissa value + 1bit hidden number
+	//logic [23:0] mantissa_y_hidden; //24 bit mantissa value + 1bit hidden number
+	//logic [22:0] mantissa_x;
+	//logic [22:0] mantissa_y;
+	assign mantissa_x = X[22:0];
+	assign mantissa_y = Y[22:0];
+	assign mantissa_x_hidden = {1'b1, X[22:0]}; 
+	assign mantissa_y_hidden = {1'b1, Y[22:0]};
+	assign mantissa =  mantissa_x_hidden * mantissa_y_hidden;
 	
 	//4. Round the 48-bit product by truncating the least significant 23-bits. You should be left with a 25-bit`rounded value' comprising 2 hidden bits and 23 mantissa bits.
 
-	logic [22:0] mantissa_truncated;
-	logic [1:0] mantissa_hidden;
+	//logic [22:0] mantissa_truncated;
+	//logic [1:0] mantissa_hidden;
 	assign mantissa_hidden = mantissa [47:46];	// assign 2 MSBs to as hidden bits
 	assign mantissa_truncated = mantissa [45:23]; // truncate the 23-LSBs	
 	
@@ -36,28 +60,60 @@ module part1(
 	//division by two, this requires the exponent to be increased by 1. In decimal, for example, 23.05 x 10^3 is
 	//the same as 2.305 x 10^4.
 	
-	logic [22:0] mantissa_final;
-	logic [7:0] exponent_final;
-	
+	//logic [22:0] mantissa_final;
+	//logic [8:0] exponent_final; //also for the special cases check
+
 	always_comb 
-		begin
+		begin	
+			mantissa_final = mantissa_truncated;
 			if (mantissa_hidden > 2'b01)
 				begin
 					logic [24:0] mantissa_shifted;
 					mantissa_shifted = {mantissa_hidden, mantissa_truncated} >> 1;	// if hidden MSB is 1 (11, 10), then shift the 25 mantissa bit (including the hidden bit) to right by 1
 					mantissa_final = mantissa_shifted [22:0];
-					exponent_final = exponent + 1'd1; // increase exponent by 1 decimal point
-				end
-			else
+					exponent_final = exponent_with_127 + 1'd1; // increase exponent by 1 decimal point
+				end			
+			//if (((exponent_x + exponent_y) >= 127) && ((exponent_x + exponent_y) < 382)) //normal cases
+				//exponent_final = exponent_with_127 - 127;
+			//Special Cases
+			//Check both Input and Output for this case: X(mantissa_x, exponent_x), Y(mantissa_y, exponent_y), Final(mantissa_final, exponent_final)
+			
+			//Zero case
+			if (((mantissa_x == 0) && (exponent_x == 0)) || ((mantissa_y == 0) && (exponent_y == 0)) || ((exponent_with_127 == 127) && (mantissa_final == 0)))
+			//if ((exponent_with_127 == 127) & (mantissa_final == 0))
 				begin
-					mantissa_final = mantissa_truncated;
-					exponent_final = exponent;	
+					exponent_final = 0;
+					mantissa_final = 0;
+					zero = 1'b1;		
 				end
+			//Underflow case
+			else if (exponent_with_127 < 127)
+				begin
+					exponent_final = 0;
+					mantissa_final = 23'b111_1111_1111_1111_1111_1111;  //set all mantissa bits to 1 just to differenciate with zero case
+					underflow = 1'b1;
+				end
+			//Not a number case
+			else if ((exponent_with_127 == 382) & (mantissa_final != 0))
+				begin
+					exponent_final = 255;
+					mantissa_final = 0;
+					nan = 1'b1;
+				end
+			//Overflow case
+			else if (exponent_with_127 >= 382)
+				begin
+					exponent_final = 255;
+					mantissa_final = 0;
+					overflow = 1'b1;
+				end
+			else 
+				exponent_final = exponent_with_127 - 127;
 		end
 	
 	//6. As the 2 most significant bits of the mantissa are now normalized, they must be 01 and can therefore be ignored. The 1 is the hidden one in the final number.
 	
-	assign result = {sign, exponent_final, mantissa_final};
+	assign result = {sign, exponent_final [7:0], mantissa_final};
 	
 endmodule
 	
--- Lab4/Part 1/part1_tb.sv
+++ Lab4/Part 1/part1_tb.sv
@@ -1,22 +1,104 @@
 `timescale 1ns/1ns
 module part1_tb();
 
-logic [31:0] X, Y, out;
+logic [31:0] X, Y, result;
+logic zero, underflow, overflow, nan;
+logic [7:0] exponent_x;
+logic [7:0] exponent_y;
+logic [8:0] exponent_with_127;
+logic [23:0] mantissa_x_hidden;
+logic [23:0] mantissa_y_hidden;
+logic [47:0] mantissa;
+logic [22:0] mantissa_truncated;
+logic [1:0] mantissa_hidden;
+logic [22:0] mantissa_final;
+logic [8:0] exponent_final;
+logic [22:0] mantissa_x;
+logic [22:0] mantissa_y;
 
-part1 DUT(X, Y, out);
+part1 DUT(X, Y, result, exponent_x, exponent_y, exponent_with_127, mantissa_x_hidden, mantissa_y_hidden, mantissa, mantissa_truncated, mantissa_hidden, mantissa_final, exponent_final,
+mantissa_x, mantissa_y, zero, underflow, overflow, nan);
 
 initial begin
+
+	zero = 0;
+	underflow = 0;
+	overflow = 0;
+	nan = 0;
+	
+	#5;
 
 	X = 32'b00000000000000000000000000000000;
 	Y = 32'b00000000000000000000000000000000;
 	
-	#10;
+	
+	#5;
+	
+	zero = 0;
+	underflow = 0;
+	overflow = 0;
+	nan = 0;
+	
+	#5;
 	
 	X = 32'b1100_0001_1001_0000_0000_0000_0000_0000;
 	Y = 32'b0100_0001_0001_1000_0000_0000_0000_0000;
 	
-	#10;
+	// Result = 1100_0011_0010_1011_0000_0000_0000_0000
 	
+	#5;
+	
+	zero = 0;
+	underflow = 0;
+	overflow = 0;
+	nan = 0;
+	
+	#5;
+	
+	//Zero case
+	X = 32'b1000_0000_0000_0000_0000_0000_0000_0000; // E = 0, M = 0
+	Y = 32'b0100_0001_0001_1111_0110_0000_0000_0000;
+
+	#5;
+	
+	zero = 0;
+	underflow = 0;
+	overflow = 0;
+	nan = 0;
+	
+	#5;
+	
+	//Underflow case
+	X = 32'b1000_1111_0000_0000_1111_1111_0000_0000; //E = 30
+	Y = 32'b0001_0100_0001_1000_0000_0000_1111_1111; //E = 40
+
+	#5;
+	
+	zero = 0;
+	underflow = 0;
+	overflow = 0;
+	nan = 0;
+	
+	#5;
+	
+	//Not-a-Number case
+	X = 32'b1000_0000_0000_0000_0000_0000_0000_0000; // Zero
+	Y = 32'b0111_1111_1000_0000_0000_0000_0000_0000; // Infinity
+	
+	#5;
+	
+	zero = 0;
+	underflow = 0;
+	overflow = 0;
+	nan = 0;
+	
+	#5;
+	
+	//Overflow case
+	X = 32'b1111_1111_1000_0000_1111_1111_0000_0000; //E = 256
+	Y = 32'b0111_1111_1001_1000_0000_0000_1111_1111; //E = 256	
+	
+	#5;
 	$stop();
 	
 end
