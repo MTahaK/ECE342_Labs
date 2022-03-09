b941603c6f3ecf9a51bef1c192656d716bc493e3
MTahaK <taha.khokhar@mail.utoronto.ca>
2022-03-09 03:19:29 -0500

    Part 2 code appears to be running correctly - just need to add measurements

---
+++ Lab6/part2-quartus/software/fixed_point/obj/default/hello_world.d
@@ -0,0 +1,23 @@
+obj/default/hello_world.o: hello_world.c \
+ ../fixed_point_bsp//drivers/inc/altera_avalon_performance_counter.h \
+ ../fixed_point_bsp//HAL/inc/alt_types.h ../fixed_point_bsp//HAL/inc/io.h \
+ ../fixed_point_bsp//HAL/inc/alt_types.h \
+ ../fixed_point_bsp//HAL/inc/sys/alt_irq.h \
+ ../fixed_point_bsp//HAL/inc/nios2.h ../fixed_point_bsp/system.h \
+ ../fixed_point_bsp/linker.h
+
+../fixed_point_bsp//drivers/inc/altera_avalon_performance_counter.h:
+
+../fixed_point_bsp//HAL/inc/alt_types.h:
+
+../fixed_point_bsp//HAL/inc/io.h:
+
+../fixed_point_bsp//HAL/inc/alt_types.h:
+
+../fixed_point_bsp//HAL/inc/sys/alt_irq.h:
+
+../fixed_point_bsp//HAL/inc/nios2.h:
+
+../fixed_point_bsp/system.h:
+
+../fixed_point_bsp/linker.h:
---
+++ Lab6/part2-quartus/software/part2_tester/obj/default/hello_world.d
@@ -0,0 +1,24 @@
+obj/default/hello_world.o: hello_world.c ../part2_tester_bsp/system.h \
+ ../part2_tester_bsp/linker.h \
+ ../part2_tester_bsp//drivers/inc/altera_avalon_performance_counter.h \
+ ../part2_tester_bsp//HAL/inc/alt_types.h \
+ ../part2_tester_bsp//HAL/inc/io.h \
+ ../part2_tester_bsp//HAL/inc/alt_types.h \
+ ../part2_tester_bsp//HAL/inc/sys/alt_irq.h \
+ ../part2_tester_bsp//HAL/inc/nios2.h
+
+../part2_tester_bsp/system.h:
+
+../part2_tester_bsp/linker.h:
+
+../part2_tester_bsp//drivers/inc/altera_avalon_performance_counter.h:
+
+../part2_tester_bsp//HAL/inc/alt_types.h:
+
+../part2_tester_bsp//HAL/inc/io.h:
+
+../part2_tester_bsp//HAL/inc/alt_types.h:
+
+../part2_tester_bsp//HAL/inc/sys/alt_irq.h:
+
+../part2_tester_bsp//HAL/inc/nios2.h:
