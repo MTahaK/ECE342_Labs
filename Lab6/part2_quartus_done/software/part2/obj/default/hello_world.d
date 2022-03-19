31c51c19406caec7e2da9e67de99c74d788c04d2
JinwooCheon-GitHub <82179219+JinwooCheon-GitHub@users.noreply.github.com>
2022-03-19 16:34:04 -0400

    part2 done

---
+++ Lab6/part2_quartus_done/software/part2/obj/default/hello_world.d
@@ -0,0 +1,22 @@
+obj/default/hello_world.o: hello_world.c ../part2_bsp/system.h \
+ ../part2_bsp/linker.h \
+ ../part2_bsp//drivers/inc/altera_avalon_performance_counter.h \
+ ../part2_bsp//HAL/inc/alt_types.h ../part2_bsp//HAL/inc/io.h \
+ ../part2_bsp//HAL/inc/alt_types.h ../part2_bsp//HAL/inc/sys/alt_irq.h \
+ ../part2_bsp//HAL/inc/nios2.h
+
+../part2_bsp/system.h:
+
+../part2_bsp/linker.h:
+
+../part2_bsp//drivers/inc/altera_avalon_performance_counter.h:
+
+../part2_bsp//HAL/inc/alt_types.h:
+
+../part2_bsp//HAL/inc/io.h:
+
+../part2_bsp//HAL/inc/alt_types.h:
+
+../part2_bsp//HAL/inc/sys/alt_irq.h:
+
+../part2_bsp//HAL/inc/nios2.h:
